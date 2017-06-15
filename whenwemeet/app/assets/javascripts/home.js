e5ac5268fd71e65bb4ecf8216bbecbbaab9ceaec
Ray Suhyun Lee <raysuhyunlee@gmail.com>
2017-06-16 02:38:13 +0900

    Add group calendar support

--- whenwemeet/app/assets/javascripts/home.js
+++ whenwemeet/app/assets/javascripts/home.js
@@ -6,5 +6,15 @@ $(document).ready(function() {
     var index = $(this).index();
     $("div.bhoechie-tab>div.bhoechie-tab-content").removeClass("active");
     $("div.bhoechie-tab>div.bhoechie-tab-content").eq(index).addClass("active");
+
+    if ($(this).data('groupid')) {
+      $('<form action="/time/get_common" method="GET"/>')
+          .append($('<input type="hidden" name="group_id" value="' + $(this).data('groupid') + '">'))
+          .appendTo($(document.body)) //it has to be added somewhere into the <body>
+          .submit();
+    } else {
+      window.location.replace('/')
+    }
+
   });
 });
