c7e7d4ac415395b110eacab69e73fcb7abb7da43
jsw <akino0604@gmail.com>
2017-06-15 22:52:10 +0900

    popup & group 코드 수정

--- whenwemeet/config/routes.rb
+++ whenwemeet/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get 'get_common_timetable/time_list'
 
+  get 'home/popup'
+
+  post 'home/groupadd'
+
   root 'home#index'
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
